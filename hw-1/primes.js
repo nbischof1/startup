fcbbd367cfe2b61fb0404a8c7e9ca8d8ac996a77
Ubuntu <ubuntu@ip-172-31-6-172.us-west-2.compute.internal>
2013-06-25 18:02:00 +0000

    fixed primes on ec2 for hw1

--- hw-1/primes.js
+++ hw-1/primes.js
@@ -1,12 +1,4 @@
-/*
- * Dies ist eine JavaScript-Umgebung.
- *
- * Geben Sie etwas JavaScript ein und führen Sie einen Rechtsklick aus oder wählen Sie aus dem Ausführen-Menü:
- * 1. Ausführen, um den ausgewählten Text zu evaluieren (Strg+R),
- * 2. Untersuchen, um den Objekt-Inspektor auf das Resultat anzuwenden (Strg+I), oder
- * 3. Anzeigen, um das Ergebnis in einem Kommentar hinter der Auswahl einzufügen. (Strg+L)
- */
-
+/* Niels Bischof, 2013-06-25 */
 
 function isPrime(number) {
     if(number<2) return false;
@@ -25,5 +17,5 @@ for(i=1; i<=100; i++) {
         primes.push(i);
     }
 }
-print('primes: ' + primes);
+console.log(''+primes);
 
